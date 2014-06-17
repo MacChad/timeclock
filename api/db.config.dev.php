d98020ddaca13b8a85b0c2318ba0fa4195b4ac6a
OpenShift guest <chad.nauman@gmail.com>
2014-06-17 10:15:04 -0400

    second commit

---
+++ api/db.config.dev.php
@@ -0,0 +1,6 @@
+<?php
+DB::$user = 'macchad';
+DB::$host = '127.8.67.1';
+DB::$password = '';
+DB::$dbName = 'c9';
+?>
---
+++ api/db.config.prod.php
@@ -0,0 +1,7 @@
+<?php
+DB::$user = 'tcuser';
+DB::$password = 'NHtvL8qttSdY7eEm';
+DB::$host = $OPENSHIFT_MYSQL_DB_HOST;
+DB::$port = $OPENSHIFT_MYSQL_DB_PORT
+DB::$dbName = 'timeclock';
+?>
