046f593a5d68b276d3b05a2ea79001e643f3c6e3
OpenShift guest <chad.nauman@gmail.com>
2014-06-17 12:43:41 -0400

    issue with prod config

--- api/db.config.prod.php
+++ api/db.config.prod.php
@@ -2,6 +2,6 @@
 DB::$user = 'tcuser';
 DB::$password = 'NHtvL8qttSdY7eEm';
 DB::$host = $OPENSHIFT_MYSQL_DB_HOST;
-DB::$port = $OPENSHIFT_MYSQL_DB_PORT
+DB::$port = $OPENSHIFT_MYSQL_DB_PORT;
 DB::$dbName = 'timeclock';
 ?>
